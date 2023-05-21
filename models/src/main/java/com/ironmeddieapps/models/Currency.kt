9a6e84b15813dd1d2a19ddc82e6ce629981c1e09
IronMeddie <mikaelofarius@gmail.com>
2023-05-21 11:22:59 +0300

    start

---
+++ models/src/main/java/com/ironmeddieapps/models/Currency.kt
@@ -0,0 +1,9 @@
+package com.ironmeddieapps.models
+
+data class Currency(
+    val Date: String,
+    val PreviousDate: String,
+    val PreviousURL: String,
+    val Timestamp: String,
+    val Valute: Valute
+)
---
+++ remote/src/main/java/com/ironmeddieapps/remote/utils/Constance.kt
@@ -0,0 +1,8 @@
+package com.ironmeddieapps.remote.utils
+
+object Constance {
+
+    const val BASE_URL = "https://www.cbr-xml-daily.ru/"
+    const val CURRENCY_JSON = "/daily_json.js"
+    const val NO_INTERNET = "No Internet"
+}
